3d43cce914318b126cba718edc053b947bdd597d
gary leong <gear@thytruth.com>
2019-02-06 19:32:59 +0800

    added first usable testable ci stuff

--- ed_stacks/_ed_configs/ec2_ci/run.py
+++ ed_stacks/_ed_configs/ec2_ci/run.py
@@ -1,8 +1,8 @@
-class Main(InstructionWrapper):
+class Main(newSchedStack):
 
-    def __init__(self,instructargs):
+    def __init__(self,stackargs):
 
-        InstructionWrapper.__init__(self,instructargs)
+        newSchedStack.__init__(self,stackargs)
 
         #Required
         required_keys = [ "DOCKER_USERNAME",
@@ -14,7 +14,8 @@ class Main(InstructionWrapper):
                           "repo_key_loc"
                           ]
 
-        for required_key in required_keys: self.parse.add_required(key=required_key)
+        for required_key in required_keys: 
+            self.parse.add_required(key=required_key)
 
         self.parse.add_required(key="DOCKER_REGISTRY",default="docker.io")
         self.parse.add_required(key="DOCKER_REPO_TYPE",default="public")
@@ -30,26 +31,26 @@ class Main(InstructionWrapper):
         self.parse.add_optional(key="commit_hash")
         self.parse.add_optional(key="commit_info")
 
+        self.stack.add_substack("elasticdev:::ed_core::run_commit_info")
+        self.stack.add_substack("elasticdev:::docker::ec2_helper_ci")
+
+        self.stack.init_substacks()
+
     def run_updatecode(self):
 
-        self.set_class_vars()
+        self.init_variables()
 
-        default_values = {}
-        default_values["commit_info"] = self.commit_info
-        instruction_name = "elasticdev:::ed_core::run_commit_info"
-        inputargs = {"name":instruction_name,
-                     "default_values":default_values}
+        inputargs = {}
+        inputargs["default_values"] = {"commit_info":self.commit_info}
         inputargs["automation_phase"] = "continuous_delivery"
         inputargs["human_description"] = 'Publish commit_info'
-        inputargs["display_hash"] = self.instructions.get_hash_object(inputargs)
-
-        return self.instructions.add_instruction(**inputargs)
+        return self.stack.run_commit_info.insert(display=True,**inputargs)
 
     def set_chk_registerdocker_attr(self):
 
         #Docker Image Name and Repo
         self.DOCKER_REPO = "{}/{}/{}".format(self.DOCKER_REGISTRY,self.DOCKER_USERNAME,self.DOCKER_REPO_NAME)
-        self.image_tag = self.instructions.get_random_string()
+        self.image_tag = self.stack.get_random_string()
         self.DOCKER_IMAGE = "{}:{}".format(self.DOCKER_REPO,self.image_tag)
 
     def run_registerdocker(self):
@@ -63,10 +64,7 @@ class Main(InstructionWrapper):
         self.parse.add_optional(key="ssh_port")
         self.parse.add_optional(key="http_port")
 
-        ###############################################
-        # Set Class Variables
-        ###############################################
-        self.set_class_vars()
+        self.init_variables()
 
         # This sets the commit info need to register the image
         # we don't put this in the parsing arguments requested 
@@ -91,49 +89,45 @@ class Main(InstructionWrapper):
         if hasattr(self,"PRE_SCRIPTS"): default_values["PRE_SCRIPTS"] = self.PRE_SCRIPTS
         if hasattr(self,"POST_SCRIPTS"): default_values["POST_SCRIPTS"] = self.POST_SCRIPTS
 
-        #############################
         # do we need to overide here?
-        #############################
         overide_values = {}
         overide_values["docker_host"] = self.docker_host
         overide_values["DOCKER_FILE"] = self.DOCKER_FILE_BUILD
         overide_values["CustomConfigGroups"] = "local:::private::{}".format(self.ELASTICDEV_CONFIG_GROUP_BUILD)
-        if hasattr(self,'ELASTICDEV_ACCESS_GROUP'): overide_values["CustomConfigGroups"] = "{} local:::private::{}".format(overide_values["CustomConfigGroups"],
-                                                                                                                           self.ELASTICDEV_ACCESS_GROUP)
+        if hasattr(self,'ELASTICDEV_ACCESS_GROUP'): 
+            overide_values["CustomConfigGroups"] = "{} local:::private::{}".format(overide_values["CustomConfigGroups"],
+                                                                                   self.ELASTICDEV_ACCESS_GROUP)
 
-        if hasattr(self,'ELASTICDEV_CONFIG_GROUP_SCRIPTS'): overide_values["CustomConfigGroups"] = "{} local:::private::{}".format(overide_values["CustomConfigGroups"],
-                                                                                                                                   self.ELASTICDEV_CONFIG_GROUP_SCRIPTS)
+        if hasattr(self,'ELASTICDEV_CONFIG_GROUP_SCRIPTS'): 
+            overide_values["CustomConfigGroups"] = "{} local:::private::{}".format(overide_values["CustomConfigGroups"],
+                                                                                   self.ELASTICDEV_CONFIG_GROUP_SCRIPTS)
 
-        instruction_name = "elasticdev:::docker::docker_helper_ci_ec2_directpull"
-
-        inputargs = {"name":instruction_name,
-                     "default_values":default_values,
+        inputargs = {"default_values":default_values,
                      "overide_values":overide_values}
 
         inputargs["automation_phase"] = "continuous_delivery"
         inputargs["human_description"] = 'Building docker container for commit_hash "{}"'.format(self.commit_hash)
-        inputargs["display_hash"] = self.instructions.get_hash_object(inputargs)
 
-        self.instructions.add_instruction(**inputargs)
+        return self.stack.ec2_helper_ci.insert(display=True,**inputargs)
         ##############################################
 
     def run(self):
     
-        self.instructions.unset_parallel()
+        self.stack.unset_parallel()
         ##############################################
-        self.instructions.add_job("registerdocker",instance_name="auto")
+        self.stack.add_job("registerdocker",instance_name="auto")
         #####################################
-        self.instructions.add_job("updatecode",instance_name="auto")
+        self.stack.add_job("updatecode",instance_name="auto")
         ##############################################
         ###Evaluating Jobs and loads
         ##############################################
-        for run_job in self.instructions.get_jobs(): eval(run_job)
+        for run_job in self.stack.get_jobs(): eval(run_job)
 
-        return self.instructions.get_results(self.instructions.instructargs.get("destroy_instance"))
+        return self.stack.get_results(self.stack.stackargs.get("destroy_instance"))
 
     def schedule(self):
 
-        sched = self.instructions.new_schedule()
+        sched = self.stack.new_schedule()
         sched.job = "updatecode"
         sched.archive.timeout = 300
         sched.archive.timewait = 60
@@ -144,9 +138,9 @@ class Main(InstructionWrapper):
         sched.automation_phase = "continuous_delivery"
         sched.human_description = "Insert commit info into run"
         sched.trigger = [ "registerdocker 2" ]
-        self.instructions.add_sched(sched)
+        self.stack.add_sched(sched)
 
-        sched = self.instructions.new_schedule()
+        sched = self.stack.new_schedule()
         sched.job = "registerdocker"
         sched.archive.timeout = 2700
         sched.archive.timewait = 120
@@ -155,13 +149,13 @@ class Main(InstructionWrapper):
         sched.conditions.frequency = "wait_last_run 60"
         sched.automation_phase = "continuous_delivery"
         sched.human_description = "Building docker container with code"
-        self.instructions.add_sched(sched)
+        self.stack.add_sched(sched)
         
         #The order of delete instances
         delete_instsuffix = [ "updatecode", "registerdocker" ]
-        self.instructions.schedule_on_delete(parallel=delete_instsuffix)
+        self.stack.schedule_on_delete(parallel=delete_instsuffix)
 
-        return self.instructions.schedules
+        return self.stack.schedules
 
         #self.parse.add_optional(key="repo_url")
         #self.parse.add_optional(key="docker_host")
