f51b1950c847f3fc4cb19706b356e59c36d9d4f6
gary leong <gear@thytruth.com>
2019-06-07 21:32:41 -0700

    update

--- stacks/_ed_configs/ec2_ondemand_ci/_main/run.py
+++ stacks/_ed_configs/ec2_ondemand_ci/_main/run.py
@@ -50,9 +50,6 @@ def run(stackargs):
         stack.logger.error(msg)
         stack.ehandle.NeedRtInput(message=msg)
 
-    # Set parallel
-    stack.set_parallel()
-
     # Create docker host
     default_values = {"size":stack.docker_host_size}
     default_values["disksize"] = stack.docker_host_disksize
@@ -65,6 +62,9 @@ def run(stackargs):
     inputargs["automation_phase"] = "continuous_delivery"
     inputargs["human_description"] = human_description
     stack.ec2_docker_host.insert(display=True,**inputargs)
+
+    # Set parallel
+    stack.set_parallel()
 
     # We add EnvVars for the Run only
     pipeline_env_var = {"COMMIT_HASH":stack.commit_hash}
@@ -99,9 +99,6 @@ def run(stackargs):
     pipeline_env_var["DOCKER_ENV_FILE"] = docker_env_file
     stack.add_metadata_to_run(pipeline_env_var,env_var=True)
 
-    # Wait to complete on host
-    stack.wait_all_instance(**{ "queue_host":"instance","max_wt":"self"})
-
     # Add cluster vars
     env_ref = "{} hostname:{}".format(stack.base_env,stack.docker_host)
     cvar_name = stack.tag
@@ -125,6 +122,9 @@ def run(stackargs):
                            stack.docker_host ]
 
     stack.add_cluster_envs(**input_args)
+
+    # Wait to complete on host
+    stack.wait_all_instance(**{ "queue_host":"instance","max_wt":"self"})
 
     #docker_host_info = stack.check_resource(name=stack.docker_host,
     #                                        resource_type="server",
