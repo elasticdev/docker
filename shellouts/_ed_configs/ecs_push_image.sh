2bb996d9c07ee0fa260b53eafb5ddbb6d9640d43
gary leong <gear@thytruth.com>
2019-06-10 14:57:54 -0700

    add basic ecs push image

---
+++ shellouts/_ed_configs/ecs_push_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export DOCKER_IMAGE=`echo "$DOCKER_IMAGE" | sed -e 's/ //g'`
+export REPO_NAME=`echo $DOCKER_IMAGE | cut -d ":" -f 1`
+export DOCKER_REPO_TAG=`echo $DOCKER_IMAGE | cut -d ":" -f 2`
+export DOCKER_REGISTRY=`echo $REPO_NAME | cut -d "/" -f 1`
+export DOCKER_REPO=`echo $REPO_NAME | cut -d "/" -f 3`
+export DOCKER_USERNAME=${DOCKER_USERNAME:=`echo $REPO_NAME | cut -d "/" -f 2`}
+
+$DOCKER_LOGIN
+
+echo "docker tag $DOCKER_REPO $REPO_NAME:latest"
+docker tag $DOCKER_REPO $REPO_NAME:latest
+
+echo "docker push $REPO_NAME:latest"
+docker push $REPO_NAME:latest
+
+echo "docker tag $DOCKER_REPO $REPO_NAME:$DOCKER_REPO_TAG"
+docker tag $DOCKER_REPO $REPO_NAME:$DOCKER_REPO_TAG
+
+echo "docker push $DOCKER_IMAGE"
+docker push $DOCKER_IMAGE
+
