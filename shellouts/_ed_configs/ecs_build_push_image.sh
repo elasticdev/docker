fbbb4ce1f2c873aa7669fdb93e83f3e49225a553
gary leong <gear@thytruth.com>
2019-06-17 20:29:14 -0700

    update

--- shellouts/_ed_configs/ecs_build_push_image.sh
+++ shellouts/_ed_configs/ecs_build_push_image.sh
@@ -7,9 +7,14 @@ export DOCKER_ENV_FILE=${DOCKER_ENV_FILE:=${DOCKER_BUILD_DIR}/.env}
 
 echo "Building for repository $REPOSITORY_URI at $DOCKER_BUILD_DIR"
 
+# Build with custom image tag
 cd $DOCKER_BUILD_DIR
-echo "execute: docker build -t $REPOSITORY_URI . "
-docker build -t $REPOSITORY_URI . || exit 1
+echo "execute: docker build -t $REPOSITORY_URI:$DOCKER_IMAGE_TAG . "
+docker build -t $REPOSITORY_URI:$DOCKER_IMAGE_TAG . || exit 1
+
+# Build/tag with tag "latest"
+echo "execute: docker build -t $REPOSITORY_URI:latest . "
+docker build -t $REPOSITORY_URI:latest . || exit 1
 
 echo "Login to repository $REPOSITORY_URI"
 $ECR_LOGIN || exit 1
@@ -18,15 +23,15 @@ echo "Pushing latest image $IMAGE_ID to repository $REPOSITORY_URI"
 echo "execute: docker push $REPOSITORY_URI"
 docker push $REPOSITORY_URI || exit 1
 
-export IMAGE_ID=$(docker images -q ${REPOSITORY_URI}| head -n 1)
-
-echo "execute: docker tag $IMAGE_ID $DOCKER_IMAGE_TAG"
-docker tag $IMAGE_ID $DOCKER_IMAGE_TAG || exit 1
-
-echo "Pushing latest image $IMAGE_ID with tag $DOCKER_IMAGE_TAG to repository $REPOSITORY_URI"
-echo "execute: docker push $DOCKER_IMAGE_TAG"
-docker push $DOCKER_IMAGE_TAG || exit 1
-
 cd -
 
+#export IMAGE_ID=$(docker images -q ${REPOSITORY_URI}| head -n 1)
+#
+#echo "execute: docker tag $IMAGE_ID $DOCKER_IMAGE_TAG"
+#docker tag $IMAGE_ID $DOCKER_IMAGE_TAG || exit 1
+#
+#echo "Pushing latest image $IMAGE_ID with tag $DOCKER_IMAGE_TAG to repository $REPOSITORY_URI"
+#echo "execute: docker push $DOCKER_IMAGE_TAG"
+#docker push $DOCKER_IMAGE_TAG || exit 1
+#cd -
 #docker tag build -t $DOCKER_REPO:$DOCKER_IMAGE_TAG . || exit 1
