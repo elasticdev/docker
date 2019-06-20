32db2a1a0bb06e88b7afbc8c75c9d87f0d55057e
gary leong <gear@thytruth.com>
2019-06-19 18:20:28 -0700

    update

--- stacks/_ed_configs/ec2_docker_host/_main/run.py
+++ stacks/_ed_configs/ec2_docker_host/_main/run.py
@@ -6,6 +6,8 @@ def run(stackargs):
     # add variables
     stack.parse.add_required(key="os_version",default="16.04")
     stack.parse.add_required(key="hostname",default="null")
+    stack.parse.add_required(key="aws_default_region",default="null")
+    stack.parse.add_required(key="region",default="null")
     stack.parse.add_required(key="key",default="first_ssh_key")
 
     stack.parse.add_optional(key="sg_label",default="null")
@@ -36,8 +38,7 @@ def run(stackargs):
                      "vpc",
                      "user",
                      "timeout",
-                     "security_group",
-                     "aws_default_region"]
+                     "security_group"]
 
     default_values = {"size":"t2.medium"}
     default_values["disksize"] = 100
@@ -48,6 +49,8 @@ def run(stackargs):
     default_values["tags"] = "docker_host docker container single_host {} {} {}".format(stack.hostname,stack.os_version,"dev")
     if stack.sg: default_values["sg"] = stack.sg
     if stack.sg_label: default_values["sg_label"] = stack.sg_label
+    if stack.aws_default_region: default_values["aws_default_region"] = stack.aws_default_region
+    if stack.region: default_values["region"] = stack.region
     
     inputargs = {"default_values":default_values,
                  "optional_keys":optional_keys}
