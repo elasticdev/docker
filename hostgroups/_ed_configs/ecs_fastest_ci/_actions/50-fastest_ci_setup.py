765ed98966f1ddde6addb31792b8de281801912d
Gary <gary@elasticdev.io>
2019-09-16 13:53:55 +0800

    update

--- hostgroups/_ed_configs/ecs_fastest_ci/_actions/50-fastest_ci_setup.py
+++ hostgroups/_ed_configs/ecs_fastest_ci/_actions/50-fastest_ci_setup.py
@@ -4,10 +4,7 @@ def default():
     env_vars = []
     shelloutconfigs = []
 
-    shelloutconfigs.append('elasticdev:::cluster_vars::create_envfile_frm_run')
     shelloutconfigs.append('elasticdev:::docker::fastest_ci_setup')
-    shelloutconfigs.append('elasticdev:::docker::docker-compose-run')
-
     task['method'] = 'shelloutconfig'
     task['metadata'] = {'env_vars': env_vars, 
                         'shelloutconfigs': shelloutconfigs 
--- hostgroups/_ed_configs/ecs_fastest_ci/_rules/order.py
+++ hostgroups/_ed_configs/ecs_fastest_ci/_rules/order.py
@@ -4,6 +4,8 @@ def default():
     ordered_tasks.append("10-pull-code.py,end")
     ordered_tasks.append("20-cluster_var_env.py,end")
     ordered_tasks.append("30-build_image.py,end")
-    ordered_tasks.append("40-fastest_setup.py,end")
+    ordered_tasks.append("40-fastest_env_file.py,end")
+    ordered_tasks.append("50-fastest_ci_setup.py,end")
+    ordered_tasks.append("60-launch_ci_builder.py,end")
 
     return ordered_tasks
