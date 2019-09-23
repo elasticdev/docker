a1a084efa13811113d558eb3b8396aea4a8674c7
Gary <gary@elasticdev.io>
2019-09-23 15:03:28 +0800

    update

--- hostgroups/_ed_configs/ecs_fastest_ci/_rules/order.py
+++ hostgroups/_ed_configs/ecs_fastest_ci/_rules/order.py
@@ -1,6 +1,7 @@
 def default():
 
     ordered_tasks=[]
+    ordered_tasks.append("5-install-base.py,end")
     ordered_tasks.append("10-pull-code.py,end")
     ordered_tasks.append("20-cluster_var_env.py,end")
     ordered_tasks.append("30-build_image.py,end")
