4bb8bb56e83e62f4ec5477c7c6a816a686ed52c4
Gary <gary@elasticdev.io>
2019-11-06 20:18:52 -0800

    update

--- execgroups/_ed_configs/ecs_fastest_ci/_chrootfiles/var/tmp/docker/run/ci/install_trivy_ubuntu.sh
+++ execgroups/_ed_configs/ecs_fastest_ci/_chrootfiles/var/tmp/docker/run/ci/install_trivy_ubuntu.sh
@@ -5,8 +5,5 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key ad
 echo deb https://aquasecurity.github.io/trivy-repo/deb $CODE_NAME main | tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update
 
-# revisit
+# revisit 342
 #apt-get install trivy -y
-
-
-
